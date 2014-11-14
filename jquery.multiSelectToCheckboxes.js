a43243d5d0a686877db1ac44c6619c5a1c164eca
ZehraNasif <zehra.nasif@gmail.com>
2014-11-14 03:18:14 -0500

    Update jquery.multiSelectToCheckboxes.js
    Modified to allow checkboxes to show up in two columns. Original version was one column.

--- jquery.multiSelectToCheckboxes.js
+++ jquery.multiSelectToCheckboxes.js
@@ -1,50 +1,52 @@
-/*
+(function ($) {
+    var methods = {
+        init: function () {
+            var baseId = "_" + $(this).attr("id");
+            var $div = $("<div>").insertAfter(this);
+            var maxWidth = 100;
+            var divCounter = 1;
+            var divstyle = "width:" + maxWidth;
 
-multiSelectToCheckboxes
+            $(this).children("option").each(function (index) {
+                if (divCounter == 1) {
+                    divstyle = "width:" + maxWidth + ";float:left";
+                }				
+                else divstyle = "";
+				
+                var $option = $(this);
+                var id = baseId + index;
+                var $checkbox = $("<input type='checkbox' id='" + id + "'/>").appendTo($div).change(function () {
 
-*/
+                    if ($(this).is(":checked")) {
+                        $option.attr("selected", "selected");
+                    } else {
+                        $option.removeAttr("selected");
+                    }
+                });
+                if ($option.is(":selected")) {
+                    $checkbox.attr("checked", "checked");
+                }
 
-(function($) {
-	var methods = {
-		init: function() {
-			var $div = $("<div/>").insertAfter(this);
-			var $ul = $("<ul/>").appendTo($div);
-			$ul.css({'list-style-type': 'none', 'padding': '0'})
-			var baseId = "_" + $(this).attr("id");
-			var maxWidth = 0;
-			$(this).children("option").each(function(index) {
-				var $option = $(this);
-				var id = baseId + index;
-				var $li = $("<li/>").appendTo($ul);
-				var $checkbox = $("<input type='checkbox' id='" + id + "'/>").appendTo($li).change(function() {
-					if ($(this).is(":checked")) {
-						$option.attr("selected", "selected");
-					} else {
-						$option.removeAttr("selected");
-					}
-				});
-				if ($option.is(":selected")) {
-					$checkbox.attr("checked", "checked");
-				}
-				$checkbox.after("<label for='" + id + "'>" + $option.text() + "</label>");
-				maxWidth = (parseInt($li.css('width') > maxWidth)) ? $li.css('width') : maxWidth;
-			});
-			
-			var divWidth = maxWidth + 100;
-			
-			$div.css({'width':divWidth + 'px', 'height': '100px', 'overflow-y': 'scroll', 'overflow-x': 'hidden', 'border': '1px solid #979797'});
-			
-			$(this).hide();
-		}
-	};
- 
-	$.fn.multiSelectToCheckboxes = function(method) {
-		if (methods[method]) {
-			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-		} else if (typeof method === 'object' || !method) {
-			return methods.init.apply(this, arguments);
-		} else {
-			$.error('Method ' + method + ' does not exist on jQuery.multiSelectToCheckboxes');
-		}
-	};
+                $checkbox.wrap("<div style='" + divstyle + "' >");
+                $checkbox.after("<label for='" + id + "' >" + $option.text() + "</label></div>");
+
+                divCounter++;
+                if (divCounter == 3) divCounter = 1;
+            });
+
+            $(this).hide();
+
+        }
+    };
+
+    $.fn.multiSelectToCheckboxes = function (method) {
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + method + ' does not exist on jQuery.multiSelectToCheckboxes');
+        }
+    };
 })(jQuery);
+
